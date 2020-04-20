3a93aef617c75e6d8484fe495a273ff54f0c07e0
sjigithub <64025411+sjigithub@users.noreply.github.com>
2020-04-20 21:52:17 +0000

    Day 1

---
+++ src/formatters.ts
@@ -0,0 +1,4 @@
+export function formatName(firstName: string, lastName: string): string {
+    return lastName + ', ' + firstName
+}
+export const PI = 3.1415;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
-
-import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+import { formatName, PI } from './formatters.ts';
+const theButton = document.getElementById('formatIt') as HTMLButtonElement;
+const firstName = document.getElementById('firstName') as HTMLInputElement;
+const lastName = document.getElementById('lastName') as HTMLInputElement;
+const formatted = document.getElementById('formatted') as HTMLHeadingElement;
+// console.log({
+//     theButton,
+//     firstName,
+//     lastName
+// });
+theButton.addEventListener('click', getNames)
+function getNames() {
+    const fName = firstName.value; // ugh!
+    const lName = lastName.value; // double ugh
+    console.log({ fName, lName })
+    const fullName = formatName(fName, lName);
+    formatted.innerHTML = fullName;
+}
 
